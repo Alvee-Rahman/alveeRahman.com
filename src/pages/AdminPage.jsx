a0d14c37ff9bfdfef17e000887e543e4814b28f2
alvee <alveerahman47@gmail.com>
2024-02-12 04:32:16 +0600

    update

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 export default function AdminPage() {
   return (
@@ -35,7 +38,7 @@ export default function AdminPage() {
 
         <div className="grid grid-cols-2  text-white">
           <div className="border-r-[1px] border-solid border-zinc-700 mt-[5px] ">
-            <h3 className="text-[1.4rem] uppercase font-bold  pt-[20px] pl-[30px] text-center">
+            <h3 className="text-[1.4rem] uppercase font-bold  pt-[20px]  text-center">
               Add new project
             </h3>
             <div>
@@ -113,11 +116,41 @@ export default function AdminPage() {
             </div>
           </div>
           <div className="border-r-[1px] border-solid border-black mt-[5px]">
-            <h3 className="text-[1.4rem] uppercase font-bold  py-[20px] pl-[30px] text-center">
+            <h3 className="text-[1.4rem] uppercase font-bold  py-[20px]  text-center">
               All your projects
             </h3>
-            <div className="mx-auto w-[650px] h-[300px]  shadow-2xl border-[1px] border-red-600 rounded-md">
-              <h3 className="uppercase pl-[50px] pt-[20px] font-bold text-[1rem] text-orange-500">prd management System</h3>
+            <div className="mx-auto w-[650px] h-[300px] border-[1px] border-red-600 rounded-2xl">
+              <h3 className="uppercase pl-[20px] pt-[20px] font-bold text-[1rem] text-orange-600">
+                prd management System
+              </h3>
+              <ul className="flex  pl-[20px] space-x-3 pt-[10px] mt-2">
+                <li className="bg-gray-800 rounded-full px-4 font-semibold text-white">html</li>
+                <li className="bg-gray-800 rounded-full px-4 font-semibold text-white">tailwind css</li>
+                <li className="bg-gray-800 rounded-full px-4 font-semibold text-white">react</li>
+                <li className="bg-gray-800 rounded-full px-4 font-semibold text-white">next.js</li>
+                <li className="bg-gray-800 rounded-full px-4 font-semibold text-white">postgresql</li>
+              </ul>
+              <p className=" pl-[20px] pt-4 font-bold text-orange-600">
+                Develope for project requirement documentation automation
+              </p>
+              <p className="pl-[20px] pt-4 text-gray-400">
+                User Dashboard Admin Dashboard Booking Stripe-Payment Review
+                Authentication Authorization Responsive
+              </p>
+              <div className="pl-[20px]  space-x-3 pt-4 ">
+                <a href="">
+                  <FontAwesomeIcon
+                    icon={faGithub}
+                    className="text-orange-800 text-[1.5rem]"
+                  />
+                </a>
+                <a href="">
+                  <FontAwesomeIcon
+                    icon={faEye}
+                    className="text-orange-800 text-[1.5rem]"
+                  />
+                </a>
+              </div>
             </div>
           </div>
         </div>
