628d16f1d8f8341426df83d41df62ca499da2579
alvee <alveerahman47@gmail.com>
2024-02-11 23:13:56 +0600

    update

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -33,34 +33,61 @@ export default function AdminPage() {
           </div>
         </div>
 
-        <div className="grid grid-cols-2">
-          <div className="border-r-[1px] border-solid border-black mt-[5px] ">
+        <div className="grid grid-cols-2 min-h-screen bg-black text-white">
+          <div className="border-r-[1px] border-solid border-zinc-700 mt-[5px] ">
             <h3 className="text-[1.4rem] uppercase font-bold  pt-[20px] pl-[30px] text-center">
               Add new project
             </h3>
             <div>
-              <form action="post" className="ml-10 mt-10 space-y-6">
-                <input
-                  type="text"
-                  placeholder="Add your project title here!"
-                  className=" w-[400px] p-[10px] rounded  border-solid border-b-2 border-zinc-800 "
-                />
+              <form action="post" className="ml-10 mt-10 ">
+                <p className="text-[1.5rem]">Add categories</p>
+                <div className="flex">
+                  <div>
+                    <input
+                      type="radio"
+                      id="frontend"
+                      name="category"
+                      className="text-red-500 focus:outline-none"
+                    />
+                    <label htmlFor="frontend">Frontend</label>
+                  </div>
+                  <div>
+                    <input type="radio" id="backend" name="category" />
+                    <label htmlFor="backend">Backend</label>
+                  </div>
+                  <div>
+                    <input type="radio" id="fullstack" name="category" />
+                    <label htmlFor="fullstack">Fullstack</label>
+                  </div>
+                </div>
+                <div className="space-y-10">
+                  <small></small>
+                  <div className="flex gap-x-10 gap-y-10">
+                    <input
+                      type="text"
+                      placeholder="Add your project title here!"
+                      className="  w-[300px] p-[10px] border-solid border-b-2 border-zinc-800 border-opacity-50 bg-zinc-900 "
+                    />
 
-                <input
-                  type="text"
-                  placeholder="Add a Note of your project"
-                  className=" w-[400px] p-[10px] rounded  border-solid border-b-2 border-zinc-800"
-                />
-                <input
-                  type="text"
-                  placeholder="Add your project git link."
-                  className=" w-[400px] p-[10px] rounded  border-solid border-b-2 border-zinc-800"
-                />
-                <input
-                  type="text"
-                  placeholder="Add  project live link."
-                  className=" w-[400px] p-[10px] rounded  border-solid border-b-2 border-zinc-800"
-                />
+                    <input
+                      type="text"
+                      placeholder="Add a Note of your project"
+                      className=" w-[300px] p-[10px]  border-solid border-b-2 border-zinc-800 border-opacity-50"
+                    />
+                  </div>
+                  <div className="flex gap-10">
+                    <input
+                      type="text"
+                      placeholder="Add your project git link."
+                      className=" w-[300px] p-[10px]  border-solid border-b-2 border-zinc-800 border-opacity-50"
+                    />
+                    <input
+                      type="text"
+                      placeholder="Add  project live link."
+                      className=" w-[300px] p-[10px]   border-solid border-b-2 border-zinc-900 border-opacity-50"
+                    />
+                  </div>
+                </div>
               </form>
             </div>
           </div>
